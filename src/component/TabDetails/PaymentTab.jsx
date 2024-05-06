43e702cb30d444a850bf9ddebab8020df25e1cc6
Sumit Anand <sumitanand287@gmail.com>
2024-05-06 07:13:35 -0700

    modal

--- src/component/TabDetails/PaymentTab.jsx
+++ src/component/TabDetails/PaymentTab.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { IoTrashBin } from "react-icons/io5";
+import Visa from '../../assets/AccountDetails/Visa.svg'
+import { IoIosAddCircleOutline } from "react-icons/io";
+import Modal from '../modal/Modal';
 const PaymentTab = () => {
+
+  const [openModal, setOpenModal] =useState("")
+  
+  const closeModal = () => {
+    setOpenModal(false);
+  };
+
+  
+  
   return (
-    <div>PaymentTab</div>
+
+
+    <div>
+      <div className=''>
+        <h1 className='text-Poppins pt-10 font-bold text-32 text-000000'>Payment methods</h1>
+         <div className='drop-shadow-md mt-4 ring-offset-112211 rounded-3xl bg-FFFFFF flex items-center gap-6 p-6'>
+           <div className='bg-039D9D w-378  rounded-2xl'>
+            <div className='flex flex-col  gap-57 px-4 py-4'>
+                <div className='flex items-start justify-between'>
+                  <div className='flex flex-col'>
+                    <span className='text-112211 text-2xl font-semibold font-Montserrat'>**** **** ****</span>
+                    <span className='text-112211 text-32 font-semibold font-Montserrat'>4321</span>
+                  </div>
+                  <div>
+                    <button><IoTrashBin className='w-6 h-6' /></button>
+                  </div>
+                </div>
+
+                <div className='flex items-center justify-between'>
+                  <div className='flex flex-col items-start'>
+                      <span className='text-112211 text-xs font-medium font-Montserrat'>Valid Thru</span>
+                      <span className='text-112211 text-xl font-semibold font-Montserrat'>02/27</span>
+                  </div>
+                  <img src={Visa} alt="" />
+                </div>
+            </div>
+
+           </div>
+           <div className='border-039D9D border-2 border-dashed w-378 h-212 flex items-center rounded-2xl'>
+              
+              <div className='flex flex-col items-center mx-141 gap-2'>
+                <button type='' onClick={()=>setOpenModal(!openModal)}>
+                   <IoIosAddCircleOutline className='w-12 h-12 text-039D9D '/> 
+               </button>
+               <span className='text-112211 text-xs font-medium font-Montserrat'>Add a new Card</span>
+              </div>
+                
+           
+           </div>
+        </div>
+      </div>
+      <div className='absolute z-50 top-3/4 pl-60'>
+        {
+           openModal ?<Modal SetCloseModal={closeModal}  />  :''
+        }
+
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/component/modal/Modal.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { IoClose } from "react-icons/io5";
+import inputVisa from '../../assets/AccountDetails/InputVisa.svg'
+import Checkbox from '@mui/material/Checkbox';
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+const Modal = ({SetCloseModal}) => {
+    const [cardNumber, setCardNumber] =useState('4321 4321 4321 4321')
+    const [expDate, setExpDate] =useState('02/27')
+    const [CVC, setCVC] =useState('123')
+    const [nameCard, setNameCard] =useState('John Doe')
+    
+    const handleCloseModal = () => {
+        // Close modal logic here
+        SetCloseModal(); // Call the closeModal function passed as prop
+      };
+    
+   
+
+  return (
+
+
+    <div className=''>
+        <div className='bg-FFFFFF shadow-md w-640 h-753.24 rounded-xl px-16 '>
+            <div className='flex justify-between pt-16'>
+                <h1 className='font-Poppins font-semibold text-40 text-000000'>Add a new Card</h1>
+                <button type='' onClick={handleCloseModal}>
+                  <IoClose className='w-6 h-6 text-000000'  />
+                </button>
+            </div>
+           <div className='w-full flex flex-col gap-6 pt-12'>
+                <div className='relative'>
+                    <label htmlFor="" className='absolute  bg-FFFFFF -top-3 ml-3 px-1'>Card Number</label>
+                    <input type="text" placeholder='' className='w-full border-79747E border   h-14 rounded px-4 py-010' value={cardNumber} onChange={(e)=>setCardNumber(e.target.value)} />
+                    <img src={inputVisa} alt="" className='absolute inline-flex top-4 w-6  -ml-10' />
+                </div>
+                <div className='grid grid-cols-2 gap-6'>
+                    <div className='relative'>
+                        <label htmlFor="" className='absolute  bg-FFFFFF -top-3 ml-3 px-1'>Exp. Date</label>
+                        <input type="text" placeholder='' value={expDate} className='w-full border-79747E border  h-14 rounded px-4 py-010' onChange={(e)=>setExpDate(e.target.value)}  />
+                    </div>
+                    <div className='relative'>
+                        <label htmlFor="" className='absolute  bg-FFFFFF -top-3 ml-3 px-1'>CVC</label>
+                        <input type="text" placeholder='' className='w-full border-79747E border   h-14 rounded px-4 py-010' value={CVC} onChange={(e)=>setCVC(e.target.value)} />
+                        
+                    </div>
+                </div>
+                <div className='relative'>
+                        <label htmlFor="" className='absolute  bg-FFFFFF -top-3 ml-3 px-1'>Name on Card</label>
+                        <input type="text" placeholder='' name='' value={nameCard} className='w-full border-79747E border   h-14 rounded px-4 py-010'  onChange={(e)=>setNameCard(e.target.value)} />
+                        
+                    </div>
+                   <div className='relative'>
+                        <label htmlFor="" className='absolute  bg-FFFFFF -top-3 ml-3 px-1'>Country or Region</label>
+                        <input type="text" placeholder='' name=''  className='w-full border-79747E border   h-14 rounded px-4 py-010' defaultValue={'India'} />     
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <Checkbox {...label} 
+                        defaultChecked 
+                        // color="default"
+                        // className="text-000000"
+                        style={{ color: '#000000' }} 
+                        />
+                        <h1>Securely save my information for 1-click checkout</h1>
+                    </div>
+                        <button type='' className='bg-8DD3BB h-12 rounded mt-4 flex items-center justify-center '>
+                          <span className='text-sm font-semibold text-112211'>
+                             Add Card
+                          </span>
+                        </button>
+                        <div>
+                            <p className='font-normal text-xs opacity-75 -mt-2 text-112211 text-center'>By confirming your subscription, you allow The Outdoor Inn Crowd Limited to charge your card for this payment and future payments in accordance with their terms. You can always cancel your subscription.</p>
+                        </div>
+                   
+           </div>
+       </div>
+  </div>
+  )
+}
+
+export default Modal
